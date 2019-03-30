c2c0e30be8eb94793fcb40eb5876aa68eff57b5b
Fenix31415 <dimkakirov43@mail.ru>
2019-03-30 14:11:16 +0300

    Long Serach Count

---
+++ WordsCount/CMakeFiles/count.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/count.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ WordsCount/CMakeFiles/words.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/words.dir/WordsFile.asm.o"
+  "words.pdb"
+  "words"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM)
+  include(CMakeFiles/words.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
